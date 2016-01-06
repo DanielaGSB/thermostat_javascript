27483c6d8988c2fe15c066532b9131250a94856b
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-06 17:26:48 +0000

    adds color to the thermostat

--- eventHandler.js
+++ eventHandler.js
@@ -6,15 +6,19 @@ window.onload = function() {
 
     var thermostat = new Thermostat();
 
-    $("#temperature").text( thermostat.getCurrentTemperature());
+      $("#temperature").text( thermostat.getCurrentTemperature());
+      $("#temperature").css('color', 'orange');
+
 
     $("#temperature_up").click( function () {
       thermostat.upButton();
+      color_generator();
       $("#temperature").text( thermostat.getCurrentTemperature());
     });
 
     $("#temperature_down").click( function () {
       thermostat.downButton();
+      color_generator();
       $("#temperature").text( thermostat.getCurrentTemperature());
     });
 
@@ -30,10 +34,19 @@ window.onload = function() {
       $("#temperature").text( thermostat.getCurrentTemperature());
     });
 
+    color_generator = function () {
+      if (thermostat.displayColour() == 'low-usage') {
+        $("#temperature").css('color', 'green');
+      }
 
+      if (thermostat.displayColour() == 'medium-usage') {
+        $("#temperature").css('color', 'orange');
+      }
 
-
-
+      if (thermostat.displayColour() == 'high-usage') {
+        $("#temperature").css('color', 'red');
+      }
+    };
 
 
 
