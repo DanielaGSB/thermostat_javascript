b5bdb4bee3456b4f9037c3fa0b2c2da18022d050
Daniela <daniela.galeote@gmail.com>
2016-01-06 17:09:05 +0000

    adds user interface with reset button and power saving on

--- eventHandler.js
+++ eventHandler.js
@@ -6,8 +6,6 @@ window.onload = function() {
 
     var thermostat = new Thermostat();
 
-    $("h1").css("color", "red");
-
     $("#temperature").text( thermostat.getCurrentTemperature());
 
     $("#temperature_up").click( function () {
@@ -19,6 +17,23 @@ window.onload = function() {
       thermostat.downButton();
       $("#temperature").text( thermostat.getCurrentTemperature());
     });
+
+    $("#power_saving_status").text( thermostat.isPowerSavingOn());
+
+    $("#power_saving_switch").click( function () {
+      thermostat.powerSavingSwitch();
+        $("#power_saving_status").text( thermostat.isPowerSavingOn());
+    });
+
+    $("#temperature_reset").click( function () {
+      thermostat.resetButton();
+      $("#temperature").text( thermostat.getCurrentTemperature());
+    });
+
+
+
+
+
 
 
 
