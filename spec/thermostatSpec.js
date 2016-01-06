fe58cb5e31cc31399e934951a58a74d0366cf15b
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-06 11:20:49 +0000

    gives thermostat a down and up button with minimum temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -12,4 +12,27 @@ describe('Thermostat', function() {
     });
   });
 
+  describe('#upButton', function() {
+    it('increases temperature', function() {
+      thermostat.upButton();
+      expect(thermostat.temperature).toEqual(21);
+    });
+  });
+
+  describe('#downButton', function() {
+
+    it('decreases temperature', function() {
+      thermostat.downButton();
+      expect(thermostat.temperature).toEqual(19);
+    });
+
+    describe('when temperature at 10C', function () {
+
+      it('raises error', function () {
+        thermostat.temperature = 10;
+        expect( function() {thermostat.downButton();}).toThrow('Temperature at minimum');
+      });
+    });
+  });
+
   });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,12 @@
-function Thermostat() {
-  this.temperature = 20;
+function Thermostat(temperature) {
+  this.temperature = temperature || 20;
 }
+
+Thermostat.prototype.upButton = function () {
+  return this.temperature++;
+};
+
+Thermostat.prototype.downButton = function () {
+  if (this.temperature <= 10) {throw 'Temperature at minimum';}
+  return this.temperature--;
+};
